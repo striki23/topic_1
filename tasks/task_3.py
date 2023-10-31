e36784112e099cb36c099687e51ff4540e6d3755
Дарья Демидова <striki06@mail.ru>
2023-10-31 14:49:38 +0300

    done

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -1,7 +1,7 @@
-string_literal = ...  # здесь Ваш код
-integer_literal = ...  # здесь Ваш код
-float_literal = ...  # здесь Ваш код
-boolean_literal = ...  # здесь Ваш код
+string_literal = 'Наступила осень.'  # здесь Ваш код
+integer_literal = 5  # здесь Ваш код
+float_literal = 5.01  # здесь Ваш код
+boolean_literal = True  # здесь Ваш код
 
 print(string_literal)
 print(integer_literal)
